25ae0b349e4ef86eec0d1eb1f35898eccbe830b6
VanjaKosanovic <94905713+VanjaKosanovic@users.noreply.github.com>
2021-12-14 19:49:15 +0100

    commit 14.12.2021 - vanja Base Test

--- src/test/java/pages/Psi.java
+++ src/test/java/pages/Psi.java
@@ -2,15 +2,15 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 
 public class Psi extends BasePage {
 
+    @FindBy (id = "ctl52_Filter1_RptFilters_ctl00_RptFilter_ctl01_LnkFilter")
     private WebElement hranaMenuItem;
 
     public Psi (ChromeDriver driver){
         super(driver);
-        this.hranaMenuItem = driver.findElementById("ctl52_Filter1_RptFilters_ctl00_RptFilter_ctl01_LnkFilter");
-
     }
 
     public Hrana clickOnHranaMenuItem (){
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,41 +1,40 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.BasePage;
 import pages.Cart;
 import pages.HomePage;
 import pages.LoginPage;
 
-public class BaseTest extends BasePage{
+public abstract class BaseTest{
 
-    public BaseTest (ChromeDriver driver){
-        super(driver);
-    }
-
-    public HomePage clickPetCentarLogo (){
-        petCentarLogo.click();
+    public HomePage clickPetCentarLogo (ChromeDriver driver){
+        BasePage page = new BasePage(driver);
+        page.petCentarLogo.click();
         return new HomePage(driver);
     }
 
-    public void clickSearchButton (){
-        waitUntilClickable(searchButton, 2);
-        searchInput.click();
+    public void clickSearchButton (ChromeDriver driver){
+        BasePage page = new BasePage(driver);
+        page.waitUntilClickable(page.searchButton, 2);
+        page.searchInput.click();
     }
 
-    public void search (String item){
-        searchInput.sendKeys(item);
-        clickSearchButton();
+    public void search (String item, ChromeDriver driver){
+        BasePage page = new BasePage(driver);
+        page.searchInput.sendKeys(item);
+        clickSearchButton(driver);
     }
 
-    public Cart clickCartLogo (){
-        cartLogo.click();
+    public Cart clickCartLogo (ChromeDriver driver){
+        BasePage page = new BasePage(driver);
+        page.cartLogo.click();
         return new Cart(driver);
     }
 
-    public LoginPage clickLoginIcon (){
-        logInIcon.click();
+    public LoginPage clickLoginIcon (ChromeDriver driver){
+        BasePage page = new BasePage(driver);
+        page.logInIcon.click();
         return new LoginPage(driver);
     }
 }
